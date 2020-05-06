c444133a64c1ea082950bc523f630c27eec9e0e0
goodriceball2100 <goodriceball2100@gmail.com>
2020-05-06 12:43:15 +0900

    setting

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -52,6 +52,7 @@ $(function(){
       contentType: false
     })
     .done(function(data){
+      
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,9 +3,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # Choose what kind of storage to use for this uploader:
   include CarrierWave::MiniMagick
-  
+
+  process resize_to_fit: [800, 800]
+
+  # Choose what kind of storage to use for this uploader:
   storage :fog
-  process resize_to_fit: [200, 200]
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
